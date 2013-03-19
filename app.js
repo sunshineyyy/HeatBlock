4181c74700dcb2703b61437ec3ada47a7210a6a4
sunshineyyy <yaoyu.yang88@gmail.com>
2013-03-19 07:46:26 +0000

    app.html

--- app.js
+++ app.js
@@ -53,6 +53,24 @@ MyApp.prototype.start = function() {
       });
 	console.log('stop heat');  
     });
+    this_app.heat_automatic.addEventListener('click',function(){
+      this_app.sendEvent('forward',{cmd: 'heatAutomatic',uuid: this_app.myuuid},
+                         function(err,resp){
+      });
+	console.log('Heating automatically');  
+    });
+    
+    this_app.get_tempset.addEventListener('click',function(){
+	
+      var temp_set_val = this_app.temp_set_val.value;
+      console.log('receive temp setting '+temp_set_val);
+	var control_command = 'desiredTemp'+'&tempset='+temp_set_val.toString();
+    this_app.sendEvent('forward',{cmd: control_command, uuid: this_app.myuuid},
+                         function(err,resp){
+      });
+	console.log('sending temperature '+temp_set_val);  
+    });
+
     this_app.dash.loadScript(
       "http://www.humblesoftware.com//static/js/flotr2.min.js",
       function(){
@@ -71,7 +89,7 @@ MyApp.prototype.update = function(){
   var http = new XMLHttpRequest();
   var td;
   var this_app = this;
-    console.log('now_pdt '+now_pdt+' now_utc '+now_epoch);
+  //console.log('now_pdt '+now_pdt+' now_utc '+now_epoch);
   http.open("GET","/?action=retrieve&uuid="+this.myuuid+"&since="+"latest");
   http.onreadystatechange=function(){
     if (http.readyState==4 && http.status == 200) {
@@ -91,7 +109,7 @@ MyApp.prototype.update = function(){
 	if (temp_array.length >20){
 	    temp_array.shift();
 	}
-        console.log('temp '+temp_array+' time '+now_pdt);
+        //console.log('temp '+temp_array+' time '+now_pdt);
 	this_app.Flotr.draw(this_app.data_div,[temp_array],{
             HtmlText: false,
 	    title: 'Temperature Record',
@@ -109,7 +127,10 @@ MyApp.prototype.update = function(){
 	//document.getElementById('current_temp').innerHTML =parseFloat(d).toFixed(2);
       this_app.current_temp.innerHTML=tem_num;
 	this_app.current_time.innerHTML = now_time.toLocaleTimeString();
-	console.log('show temp ' +occupy);
+	//var temp_set_val = this_app.temp_form.submit();
+	var temp_set_val = this_app.temp_set_val.value;
+	console.log('show occupy ' +occupy);
+	console.log('show setting ' +temp_set_val);
 	if (occupy==1){
 	this_app.occupy_info.innerHTML='Occupied';
 	}
@@ -129,11 +150,15 @@ MyApp.prototype.getAllElements = function(){
   this.data_div = this.getElement("data");
   this.stop_button = this.getElement("stop");
   this.start_button = this.getElement("start");
-    this.heat_on =this.getElement("heaton");
-    this.heat_off = this.getElement("heatoff");
+  this.heat_on =this.getElement("heaton");
+  this.heat_off = this.getElement("heatoff");
+  this.heat_automatic = this.getElement("heat_automatic");
+  this.get_tempset =this.getElement("enter");
   this.current_temp = document.getElementById('current_temp');
   this.occupy_info = document.getElementById('occupy_info');
   this.current_time = document.getElementById('current_time');
+  this.temp_set_val = document.getElementById ('temp_set_val');
+  //this.temp_form = document.getElementById ('temp_form');
 };
 
 //spec says app needs to be named App
